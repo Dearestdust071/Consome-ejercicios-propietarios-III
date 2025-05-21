607f7960a3ccc28fb9585666c96e53a260614414
Dust <dearestdust071@gmail.com>
2025-05-21 17:09:58 -0600

    21 mayo -J feat Ejemplo Serialize

---
+++ Consome/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text.Json;
+
+public class Libro
+{
+    public string Titulo { get; set; }
+    public string Autor { get; set; }
+    public int Publicacion { get; set; }
+}
+
+public class main
+{
+    static void Main(string[] args)
+    {
+        var Libros = new List<Libro>
+        {
+            new Libro
+            {
+                Titulo = "Cien anios de soledad",
+                Autor = "García Márquez",
+                Publicacion = 1967,
+            },
+            new Libro
+            {
+                Titulo = "1984",
+                Autor = "George Orwell",
+                Publicacion = 1949,
+            },
+        };
+
+        var serializedJson = JsonSerializer.Serialize(Libros);
+        Console.WriteLine(serializedJson);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Proyecto MENUDO 
+## Hecho en dotnet contendra diferentes ejercicios hechos en clase y como tareas
